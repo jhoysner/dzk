2aca2d1d82c0f73a87b5627601b99b120f2ef4ea
Carmelo Fallone <carmelo.ve@gmail.com>
2018-10-02 17:32:10 -0400

    unification front vuejs and back laravel

---
+++ resources/assets/js/components/layouts/Footer.vue
@@ -0,0 +1,110 @@
+<template>
+  <div id="">
+    <!-- start footer Area -->
+    <footer class="footer-area section-gap">
+        <div class="container">
+            <div class="row footer-top-wrap">
+                <div class="col-lg-2 col-md-4 col-sm-6">
+                    <div class="single-footer-widget">
+                        <h4 class="text-white text-uppercase">Company</h4>
+                        <ul class="widget-menu-list">
+                            <li><a href="#">About us</a></li>
+                            <li><a href="#">Life at our company</a></li>
+                            <li><a href="#">Giving Back</a></li>
+                            <li><a href="#">Meet the Team</a></li>
+                            <li><a href="#">clubExpert</a></li>
+                            <li><a href="#">ClubBootstrap</a></li>
+                            <li><a href="#">Icofont</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-4 col-sm-6">
+                    <div class="single-footer-widget">
+                        <h4 class="text-white text-uppercase">Products</h4>
+                        <ul class="widget-menu-list">
+                            <li><a href="#">PSD Template</a></li>
+                            <li><a href="#">HTML5 Template</a></li>
+                            <li><a href="#">Wordpress Theme</a></li>
+                            <li><a href="#">Joomla Extensions</a></li>
+                            <li><a href="#">Matex pagebuilder </a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-4 col-sm-6">
+                    <div class="single-footer-widget">
+                        <h4 class="text-white text-uppercase">Supports</h4>
+                        <ul class="widget-menu-list">
+                            <li><a href="#">Documentation</a></li>
+                            <li><a href="#">Support Forum</a></li>
+                            <li><a href="#">Support Policy</a></li>
+                            <li><a href="#">F.A.Qs</a></li>
+                            <li><a href="#">clubExpert</a></li>
+                            <li><a href="#">Contact Form </a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-4 col-sm-6">
+                    <div class="single-footer-widget">
+                        <h4 class="text-white text-uppercase">Terms of Uses</h4>
+                        <ul class="widget-menu-list">
+                            <li><a href="#">Terms of Uses</a></li>
+                            <li><a href="#">Copyright Notice</a></li>
+                            <li><a href="#">Refund Policy</a></li>
+                            <li><a href="#">Privacy Policy  </a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="single-footer-widget">
+                        <h4 class="text-white text-uppercase">Newsletter</h4>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </p>
+                        <div class="newsletter-form" id="mc_embed_signup">
+                            <form action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01" method="get">
+                                <div class="d-flex form-wrap">
+                                    <div class="wrap">
+                                        <input class="form-control" name="EMAIL" placeholder="Email address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email address'" required="" type="email">
+                                    </div>
+                                    <button class="submit-btn"><i class="icon icon-magnifier"></i></button>
+                                </div>
+                                <div class="info mt-20"></div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row footer-bottom-wrap">
+                <div class="col-lg-8 footer-bottom-left">
+                    <p class="text-white">&copy; 2018 <a href="#">Codethemes</a>. All Rights Reserved.</p>
+                    <p>
+                        MCSE boot camps have its supporters and its detractors. Some people do not understand why you should have to spend money on boot camp when you can get.
+                    </p>
+                </div>
+                <div class="col-lg-4 footer-bottom-right">
+                    <p class="text-white">Social Media Reach</p>
+                    <ul>
+                        <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                        <li><a href="#"><i class="fa fa-snapchat-ghost" aria-hidden="true"></i></a></li>
+                        <li><a href="#"><i class="fa fa-github" aria-hidden="true"></i></a></li>
+                        <li><a href="#"><i class="fa fa-slack" aria-hidden="true"></i></a></li>
+                        <li><a href="#"><i class="fa fa-dribbble" aria-hidden="true"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- End footer Area -->
+  </div>
+</template>
+<script>
+export default {
+  name: "",
+  data: () => ({
+
+  })
+}
+</script>
+<style lang="scss" scoped>
+</style>
---
+++ resources/views/layouts/removido/section_counter.blade.php
@@ -0,0 +1,51 @@
+<!-- Start counter-section -->
+<section class="counter-section section-gap">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-md-6 col-sm-6">
+                <div class="single-counter d-flex flex-row align-items-center">
+                    <div class="ico">
+                        <i class="icons icon-notebook"></i>
+                    </div>
+                    <div class="count-wrap">
+                        <h1>03K+</h1> <p>category.htmlp>Themes & templates</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6">
+                <div class="single-counter d-flex flex-row align-items-center">
+                    <div class="ico">
+                        <i class="icons icon-people"></i>
+                    </div>
+                    <div class="count-wrap">
+                        <h1>01M+</h1>
+                        <p>Registered Users</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6">
+                <div class="single-counter d-flex flex-row align-items-center">
+                    <div class="ico">
+                        <i class="icons icon-cloud-download"></i>
+                    </div>
+                    <div class="count-wrap">
+                        <h1>100K+</h1>
+                        <p>Downloads</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6">
+                <div class="single-counter d-flex flex-row align-items-center">
+                    <div class="ico">
+                        <i class="icons icon-earphones-alt"></i>
+                    </div>
+                    <div class="count-wrap">
+                        <h1>25K+</h1>
+                        <p>Tickets Resolved</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End counter-section -->

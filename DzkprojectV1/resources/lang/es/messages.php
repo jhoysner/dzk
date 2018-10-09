e0c79e844d252b729ef943669c74f18ffb9a9609
German Gutierrez <germangutierrezv@gmail.com>
2018-10-09 23:52:55 +0000

    Registro de usuario

---
+++ DzkprojectV1/resources/lang/es/messages.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'confirmed'            => 'La :attribute no coinciden.',
+    'date'                 => 'The :attribute no es una fecha válida.',
+    'email'                => 'El :attribute debe ser un correo electrónico válido.',
+    'image'                => 'El :attribute debe ser una imagen.',
+    'integer'              => 'El :attribute debe ser un entero.',
+    'max'                  => [
+        'numeric' => 'El :attribute no puede mayor a  :max.',
+    ],
+    'min'                  => [
+        'numeric' => 'El :attribute debe tener un minimo de :min.',
+        'string'  => 'El :attribute debe tener un minimo de :min caracteres.',
+    ],
+    'numeric'              => 'El :attribute debe ser un número.',
+    'required'             => 'El campo :attribute es requerido.',
+    'size'                 => [
+        'numeric' => 'El :attribute debe ser :size.',
+        'string'  => 'El :attribute debe ser :size caracteres.',
+    ]
+];

b196a05aa47af23a70b9d68fde04fa2e3869f87d
Jhoysner <jhoysner@hotmail.com>
2018-11-26 20:00:47 -0400

    fixed

--- DzkprojectV1/app/Http/Controllers/Follow/FollowController.php
+++ DzkprojectV1/app/Http/Controllers/Follow/FollowController.php
@@ -112,7 +112,13 @@ class FollowController extends Controller
     public function getUserCommerces($user)
     {
 
-        $commerces = UserHasCommerce::where('users_id', $user)->with('commerces')->get();
+          $commerces = User::with(['commercesUser' => function($query) {
+                                    $query->with('commerces');
+                        }])
+                        ->where('id',$user)->get(); 
+
+
+        // $commerces = UserHasCommerce::where('users_id', $user)->with('commerces')->get();
 
         return response()->json(['data'=>$commerces], 200);
     }
